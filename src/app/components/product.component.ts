4f8d88565025312c630992b01e6dbb1a6e37cc05
Sagit Gutiérrez <jozagust@hotmail.com>
2020-12-14 22:31:51 -0600

    componentes y decoradores

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Product } from '../product.model'
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html'
+})
+export class ProductComponent {
+
+product: Product = {
+    id: '5',
+    image: 'assets/images/camiseta.png',
+    title: 'Camiseta',
+    price: 8000,
+    description: 'bla bla bla bla bla'
+}
+
+}
