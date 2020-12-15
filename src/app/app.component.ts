ac9577e2f7ba76ee8686c1c885ebe0579f16d9b4
Sagit Gutiérrez <jozagust@hotmail.com>
2020-12-15 00:14:10 -0600

    cliclo de vida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,4 +54,8 @@ export class AppComponent {
   deleteItem(index: number) {
     this.items.splice(index, 1);
   }
+  clickProduct(id: number) {
+    console.log('product')
+    console.log(id)
+  }
 }
--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges,
+    SimpleChanges,
+    OnInit,
+    DoCheck,
+    OnDestroy
+
+} from '@angular/core';
 import { Product } from '../product.model'
+
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnInit, DoCheck, OnDestroy {
 
-product: Product = {
-    id: '5',
-    image: 'assets/images/camiseta.png',
-    title: 'Camiseta',
-    price: 8000,
-    description: 'bla bla bla bla bla'
-}
+    @Input() product!: Product;
+    @Output() productClicked: EventEmitter<any> = new EventEmitter();
+    
+    constructor() {
+        console.log(' constructor')
+    }
+
+    // ngOnChanges(changes: SimpleChanges): void {
+    //     console.log('OnChanges ')
+    //     console.log(changes)
+    // }
+
+    ngOnInit() {
+        console.log(' 3 ngOnInit');
+    }
+
+    ngDoCheck() {
+       console.log('ngDoCheck')
+    }
+    ngOnDestroy() {
+        console.log('ngOnDestroy')
+    }
+    addCart() {
+        console.log('add carrito');
+        this.productClicked.emit(this.product.id)
+    }
 
 }
